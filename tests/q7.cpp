6650f73725ece5cba48f111d7b8e013eac4fae41
Daniel Lucena <rlucena.daniel@gmail.com>
2025-11-17 22:33:14 -0300

    adiciona teste com o grafo passado para o trabalho

--- tests/q7.cpp
+++ tests/q7.cpp
@@ -10,6 +10,8 @@
 
 int main() {
 
+    // Teste do algoritmo de Floyd-Warshall, com exemplo do slide
+
     std::cout << "Creating graph from file 'digraph-floyd-warshall.txt'...\n";
     DirectedAdjacencyMatrixGraph<char> graph;
     std::vector<std::vector<double>> weights;
@@ -18,6 +20,16 @@ int main() {
     print_weights_matrix(weights, graph);
     auto results = floyd_warshall(graph, weights);
     print_floyd_warshall_result(results, graph);
+
+    // Teste do algoritmo de Floyd-Warshall, com exemplo passado no trabalho
+    std::cout << "Creating graph from file 'digraph.txt'...\n";
+    DirectedAdjacencyMatrixGraph<int> graph2;
+    std::vector<std::vector<double>> weights2;
+    populate_graph_weighted_from_file("data/digraph.txt", graph2, weights2);
+    graph2.print();
+    print_weights_matrix(weights2, graph2);
+    auto results2 = floyd_warshall(graph2, weights2);
+    print_floyd_warshall_result(results2, graph2);
 
     return 0;
 }
